2dcd630758b08cbedd1d41839a764042cde81086
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-03-29 18:24:56 -0300

    set env and comments

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
 # bashtest
+[![CI/CD](https://github.com/heitorpolidoro/bashtest/actions/workflows/ci_cd.yml/badge.svg)](https://github.com/heitorpolidoro/bashtest/actions/workflows/ci_cd.yml)
+![GitHub last commit](https://img.shields.io/github/last-commit/heitorpolidoro/bashtest)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=heitorpolidoro_bashtest&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=heitorpolidoro_bashtest)
+
+[![Latest](https://img.shields.io/github/release/heitorpolidoro/bashtest.svg?label=latest)](https://github.com/heitorpolidoro/bashtest/releases/latest)
+![GitHub Release Date](https://img.shields.io/github/release-date/heitorpolidoro/bashtest)
+
+![GitHub](https://img.shields.io/github/license/heitorpolidoro/bashtest)
+
 Functions to make test bash/shell script easy
 
+To use:
 ```shell
-source <(curl -s )
+source <(curl -s https://raw.githubusercontent.com/heitorpolidoro/bashtest/master/bashtest.sh)
 ```
+
+Available functions:
+### `assert()`
+```
+Return true if 2 strings are equals or false otherwise printing the values, 
+actual and expected, the file and the line the assert was called
+ARGUMENTS:
+  actual: The string you are checking
+  expected: The string, or regex, you expected that match equals with actual
+OUTPUTS:
+  If the 2 string doesn't match, print the file, function name and line where the assert was called, and
+the expected and actual parameters
+RETURN:
+  true if the 2 strings have a match, false otherwise
+```
+### `mock()`
+```
+Mock a command or a function printing the received parameters
+ARGUMENTS:
+  function_name: The of the function, or command, to mock
+OUTPUTS:
+  The parameters to the function
+```
+### `set_env()`
+```
+Set an environment variable to use only in the test case.
+ARGUMENTS:
+ name: The name of the environment variable
+ value: The value of the variable
+```
+### `run_tests()`
+```
+Run all the functions starting with "test_"
+OUTPUTS:
+ The result of each test
+RETURN:
+ 0 if all the tests results is "OK", 1 otherwise```
--- bashtest.sh
+++ bashtest.sh
@@ -1,23 +1,77 @@
 #!/bin/bash
+# Set of functions to allow create tests for bash/shell scripts
+
 # Enable expanding aliases in this script
 shopt -s expand_aliases
+# Array of ENV vars. Used to unset ENV vars after each test
 TEMP_ENVS=()
-
+###################################################################################################
+# Return true if 2 strings are equals or false otherwise printing the values, actual and expected,
+# the file and the line the assert was called
+# ARGUMENTS:
+#   actual: The string you are checking
+#   expected: The string, or regex, you expected that match equals with actual
+# OUTPUTS:
+#   If the 2 string doesn't match, print the file, function name and line where the assert was
+#   called, and the expected and actual parameters
+# RETURN:
+#   true if the 2 strings have a match, false otherwise
+###################################################################################################
 assert() {
+	actual=$1
+	expected=$2
 	# shellcheck disable=SC2053
-	if [[ $1 = $2 ]]; then
+	if [[ $actual = $expected ]]; then
 		true
 	else
 		echo "${BASH_SOURCE[1]} failed in function '${FUNCNAME[1]}' at line ${BASH_LINENO[0]}."
-		echo -e "EXPECTED:\n$2\nACTUAL:\n$1\n"
+		echo -e "EXPECTED:\n${expected}\nACTUAL:\n${actual}\n"
 		false
 	fi
 }
+###################################################################################################
+# Mock a command or a function printing the received parameters
+# ARGUMENTS:
+#   function_name: The of the function, or command, to mock
+# OUTPUTS:
+#   The parameters to the function
+###################################################################################################
+mock() {
+	function_name=$1
+	mocked_return() {
+		echo "###params=$*###"
+	}
 
+	# shellcheck disable=SC2139
+	# shellcheck disable=SC2140
+	alias "$function_name"="echo -n Mocked $function_name; mocked_return"
+}
+
+###################################################################################################
+# Set an environment variable to use only in the test case.
+# ARGUMENTS:
+#   name: The name of the environment variable
+#   value: The value of the variable
+###################################################################################################
+set_env() {
+	env_name=$1
+	env_value=$2
+
+	export "$env_name"="$env_value"
+	TEMP_ENVS+=("$env_name")
+}
+
+###################################################################################################
+# Run all the functions starting with "test_"
+# OUTPUTS:
+#   The result of each test
+# RETURN:
+#   0 if all the tests results is "OK", 1 otherwise
+###################################################################################################
 run_tests() {
 	error_code=0
 	for test_function in $(declare -F); do
-		if [[ "$test_function" = "test"* ]]; then
+		if [[ "$test_function" = "test_"* ]]; then
 			if $test_function; then
 				result="OK"
 			else
@@ -32,22 +86,4 @@ run_tests() {
 		fi
 	done
 	exit $error_code
-}
-
-mock() {
-	mocked_return() {
-		echo "###params=$*###"
-	}
-
-	# shellcheck disable=SC2139
-	# shellcheck disable=SC2140
-	alias "$1"="echo -n Mocked $1; mocked_return"
-}
-
-set_env() {
-	env_name=$1
-	env_value=$2
-
-	export "$env_name"="$env_value"
-	TEMP_ENVS+=("$env_name")
 }
